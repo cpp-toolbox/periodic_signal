674b8efdba0a181166684a0d4e7ad067c6ac3b11
cuppajoeman <ccn@cuppajoeman.com>
2025-01-30 14:52:53 -0500

    style: remove useless doctrings

--- periodic_signal.hpp
+++ periodic_signal.hpp
@@ -8,22 +8,8 @@
  */
 class PeriodicSignal {
   public:
-    /**
-     * @brief Constructs a PeriodicSignal with the specified rate limit.
-     * @param rate_limit_hz The rate limit in Hertz (signals per second).
-     */
     explicit PeriodicSignal(int rate_limit_hz);
-
-    /**
-     * @brief Processes the signal and determines whether it is time to emit the next signal.
-     * @return True if the signal is emitted, false otherwise.
-     */
     bool process_and_get_signal();
-
-    /**
-     * @brief Gets the time difference since the last signal processing.
-     * @return The time difference in seconds as a double.
-     */
     double get_last_delta_time() const;
 
   private:
