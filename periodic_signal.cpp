cd57ce6660eb52a5b4a403ef481e6c638fe19d97
cuppajoeman <ccn@cuppajoeman.com>
2025-02-18 06:14:36 -0500

    feat: add perfect delta operation mode

--- periodic_signal.cpp
+++ periodic_signal.cpp
@@ -1,23 +1,30 @@
 #include "periodic_signal.hpp"
+#include <iostream>
+#include <ostream>
 
-PeriodicSignal::PeriodicSignal(int rate_limit_hz)
+using std::chrono::duration;
+
+PeriodicSignal::PeriodicSignal(int rate_limit_hz, OperationMode mode)
     : period_duration(std::chrono::milliseconds(1000 / rate_limit_hz)),
-      last_signal_time(std::chrono::steady_clock::now()), last_delta_time(0.0) {}
+      last_signal_time(std::chrono::steady_clock::now()), last_delta_time(0.0), mode(mode) {}
 
 bool PeriodicSignal::process_and_get_signal() {
     auto now = std::chrono::steady_clock::now();
     auto time_since_last_on_signal = now - last_signal_time;
 
-    // Update the last delta time in seconds
-    last_delta_time = std::chrono::duration_cast<std::chrono::duration<double>>(time_since_last_on_signal).count();
+    /*std::cout << "time since last on signal: "*/
+    /*          << std::chrono::duration_cast<duration<double>>(time_since_last_on_signal).count() << std::endl;*/
 
     if (time_since_last_on_signal >= period_duration) {
-        // Calculate the leftover time after the last full period
-        auto leftover_time = time_since_last_on_signal % period_duration;
-
-        // Adjust last_signal_time to "pay forward" the leftover time into the next computation
-        // think of it as "moving the ticker back"
-        last_signal_time = now - leftover_time;
+        if (mode == OperationMode::PERFECT_DELTAS) {
+            last_delta_time = std::chrono::duration_cast<std::chrono::duration<double>>(period_duration).count();
+            auto leftover_time = time_since_last_on_signal % period_duration;
+            last_signal_time = now - leftover_time;
+        } else {
+            last_delta_time =
+                std::chrono::duration_cast<std::chrono::duration<double>>(time_since_last_on_signal).count();
+            last_signal_time = now;
+        }
         return true;
     } else {
         return false;
--- periodic_signal.hpp
+++ periodic_signal.hpp
@@ -6,13 +6,20 @@
 /**
  * @brief A class for generating periodic signals based on a specified rate.
  */
+
+enum class OperationMode {
+    PERFECT_DELTAS,
+    MEASURED_DELTAS,
+};
+
 class PeriodicSignal {
   public:
-    explicit PeriodicSignal(int rate_limit_hz);
+    explicit PeriodicSignal(int rate_limit_hz, OperationMode mode);
     bool process_and_get_signal();
     double get_last_delta_time() const;
 
   private:
+    OperationMode mode;
     std::chrono::milliseconds period_duration;              ///< The time interval between signals.
     std::chrono::steady_clock::time_point last_signal_time; ///< The time when the last signal was emitted.
     double last_delta_time; ///< The time difference since the last processing in seconds.
